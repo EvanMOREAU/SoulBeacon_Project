a3a26ff66e801f67389d97b3de728b50fa4eeae3
Evan Moreau <evan.moreau@etik.com>
2024-09-24 17:26:01 +0200

    First Commit

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+// use Faker\Factory;
+
+class UserFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $superAdmin = new User();
+        $superAdmin->setEmail('admin@domaine.tld');
+        $plaintextPassword = 'admin';
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $superAdmin,
+            $plaintextPassword
+        );
+        $superAdmin->setPassword($hashedPassword);
+        $superAdmin->setRoles(['ROLE_SUPER_ADMIN']);
+        $superAdmin->setVerified(true);
+
+        $manager->persist($superAdmin);
+        $manager->flush();
+    }
+}
